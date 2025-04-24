844549975a7a95f2300216b8ad28d3a16ff438cc
Devansh Karnwal <devansh10.karnwal@gmail.com>
2025-04-24 22:34:27 +0530

    Preference Update

--- MediUserApp/app/src/main/java/com/example/mediuserapp/navigation/navigate.kt
+++ MediUserApp/app/src/main/java/com/example/mediuserapp/navigation/navigate.kt
@@ -8,6 +8,7 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
@@ -58,7 +59,7 @@ fun navigate(viewModel: MyViewModel) {
         Routes.WaitingScreen::class.qualifiedName
     )
     val userId = remember { mutableStateOf("") }
-    val isLoadind = remember { mutableStateOf(false) }
+    val isLoading = remember { mutableStateOf(false) }
     val startScreen = remember(userId.value) {
         if (userId.value.isEmpty()) {
             Routes.SignUp
@@ -69,95 +70,102 @@ fun navigate(viewModel: MyViewModel) {
     val coroutineScope = rememberCoroutineScope()
     LaunchedEffect(1) {
         coroutineScope.launch(Dispatchers.IO) {
+            isLoading.value = true
             userId.value = viewModel.getUserIdDirectly().toString()
+            isLoading.value = false
         }
     }
-    Scaffold(
-        modifier = Modifier.fillMaxSize(),
-        bottomBar = {
-            if (hideBottomBarRoutes.none { currentRoute?.startsWith(it.orEmpty()) == true }) {
-                NavigationBar {
+    if(isLoading.value){
+        CircularProgressIndicator()
+    }
+    else {
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+            bottomBar = {
+                if (hideBottomBarRoutes.none { currentRoute?.startsWith(it.orEmpty()) == true }) {
+                    NavigationBar {
 
-                    bottomItems.forEachIndexed { index, item ->
-                        NavigationBarItem(
-                            selected = selected == index,
-                            onClick = {
-                                selected = index
-                                when (selected) {
-                                    0 -> navController.navigate(Routes.userAvailableProductRoute)
-                                    1 -> navController.navigate(Routes.addProductOrderRoute)
-                                    2 -> navController.navigate(Routes.ordersRoute)
-                                    3 -> navController.navigate(Routes.userProfileRoute)
+                        bottomItems.forEachIndexed { index, item ->
+                            NavigationBarItem(
+                                selected = selected == index,
+                                onClick = {
+                                    selected = index
+                                    when (selected) {
+                                        0 -> navController.navigate(Routes.userAvailableProductRoute)
+                                        1 -> navController.navigate(Routes.addProductOrderRoute)
+                                        2 -> navController.navigate(Routes.ordersRoute)
+                                        3 -> navController.navigate(Routes.userProfileRoute)
 
-                                }
-                            },
-                            alwaysShowLabel = true,
-                            label = {
-                                Text(text = item.name)
-                            },
-                            icon = { Icon(imageVector = item.icon, contentDescription = null) }
-                        )
+                                    }
+                                },
+                                alwaysShowLabel = true,
+                                label = {
+                                    Text(text = item.name)
+                                },
+                                icon = { Icon(imageVector = item.icon, contentDescription = null) }
+                            )
+                        }
                     }
                 }
             }
-        }
-    ) { innerPadding ->
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(innerPadding)
-        ) {
-            NavHost(navController = navController, startDestination = startScreen) {
+        ) { innerPadding ->
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding)
+            ) {
+                NavHost(navController = navController, startDestination = startScreen) {
 
-                composable<Routes.SignUp> {
-                    SignUpUi(viewModel, navController)
+                    composable<Routes.SignUp> {
+                        SignUpUi(viewModel, navController)
+                    }
+
+                    composable<Routes.Login> {
+                        LoginUi(viewModel, navController)
+                    }
+
+                    composable<Routes.WaitingScreen> {
+                        val data = it.toRoute<Routes.WaitingScreen>()
+                        waitingScreen(
+                            viewModel = viewModel,
+                            userId = data.userId,
+                            navController = navController
+                        )
+                    }
+                    composable<Routes.HomeScreen> {
+                        val data = it.toRoute<Routes.HomeScreen>()
+                        homeScreenUi(
+                            userId = data.userId,
+                        )
+                    }
+
+
+                    composable<Routes.specificProductScreenRoute> {
+                        val args = it.toRoute<Routes.specificProductScreenRoute>()
+                        SpecificProductScreenUi(args.productId, viewModel)
+                    }
+
+                    composable<Routes.addProductOrderRoute> {
+                        AddProductScreenUi(viewModel, navController)
+                    }
+                    composable<Routes.ordersRoute> {
+                        OrderScreenUi(viewModel, navController)
+                    }
+
+                    composable<Routes.userAvailableProductRoute> {
+                        UserAvailableProductUi(viewModel, navController)
+                    }
+
+                    composable<Routes.userProfileRoute> {
+                        userProfileUi(viewModel)
+                    }
+
                 }
 
-                composable<Routes.Login> {
-                    LoginUi(viewModel, navController)
-                }
-
-                composable<Routes.WaitingScreen> {
-                    val data = it.toRoute<Routes.WaitingScreen>()
-                    waitingScreen(
-                        viewModel = viewModel,
-                        userId = data.userId,
-                        navController = navController
-                    )
-                }
-                composable<Routes.HomeScreen> {
-                    val data = it.toRoute<Routes.HomeScreen>()
-                    homeScreenUi(
-                        userId = data.userId,
-                    )
-                }
-
-
-                composable<Routes.specificProductScreenRoute> {
-                    val args = it.toRoute<Routes.specificProductScreenRoute>()
-                    SpecificProductScreenUi(args.productId, viewModel)
-                }
-
-                composable<Routes.addProductOrderRoute> {
-                    AddProductScreenUi(viewModel, navController)
-                }
-                composable<Routes.ordersRoute> {
-                    OrderScreenUi(viewModel, navController)
-                }
-
-                composable<Routes.userAvailableProductRoute> {
-                    UserAvailableProductUi(viewModel, navController)
-                }
-
-                composable<Routes.userProfileRoute> {
-                    userProfileUi(viewModel)
-                }
 
             }
 
-
         }
-
     }
 }
 
